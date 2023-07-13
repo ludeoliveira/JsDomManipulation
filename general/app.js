16b746e77ead304c1bd7f4c8700b96b31b2a58cb
Lu de Oliveira <luludeolive.dev@gmail.com>
2023-07-13 18:02:18 -0400

    add more methods

--- general/app.js
+++ general/app.js
@@ -1,26 +1,70 @@
-// Dom Manipulation
+// // 1. Dom Manipulation
 
-//GetElementById()
+// //GetElementById()
 
-const title = document.getElementById('main-heading');
-console.log(title);
+// const title = document.getElementById('main-heading');
+// console.log(title);
 
-//GetElementByClassName
+// //GetElementByClassName
 
-const listItem = document.getElementsByClassName('list-items');
-console.log(listItem);
+// const listItem = document.getElementsByClassName('list-items');
+// console.log(listItem);
 
-//GetElementByTagName
+// //GetElementByTagName
 
-const listItem2 = document.getElementsByTagName('li');
-console.log(listItem2);
+// const listItem2 = document.getElementsByTagName('li');
+// console.log(listItem2);
 
-//querySelector() - select the first item that matchs the selector given
+// //querySelector() - select the first item that matchs the selector given
 
-const container = document.querySelector('div');
-console.log(container);
+// const container = document.querySelector('div');
+// console.log(container);
 
-//querySelectorAll() - select the first item that matchs the selector given
+// //querySelectorAll() - select the first item that matchs the selector given
 
-const containers = document.querySelectorAll('div');
-console.log(containers);
+// const containers = document.querySelectorAll('div');
+// console.log(containers);
+
+
+//2. manipulate style of elements
+
+// const title = document.querySelector('#main-heading');
+
+// title.style.color = 'red';
+
+// const listItems = document.querySelectorAll('.list-items');
+
+// for (i = 0; i < listItems.length; i++) {
+//     listItems[i].style.fontSize = '20px';
+// }
+
+//3.creating elements
+
+//const ul = document.querySelector('ul');
+//const li = document.createElement('li');
+
+//insert li element inside of existent ul element
+
+//ul.append(li);
+
+// Modifying the text
+
+li.innerText = 'X-men';
+
+// Modifying Attributes and classes
+
+li.setAttribute('id', 'main-heading'); // gets atribute id with the name main-heading
+li.removeAttribute('id');
+
+li.classList.add('list-items');
+
+// Remove Elements
+
+li.remove();
+
+// 4. Traverse the Dom
+//Parent node traversal
+
+let ul = document.querySelector('ul');
+console.log(ul.parentNode);
+console.log(ul.parentElement);
